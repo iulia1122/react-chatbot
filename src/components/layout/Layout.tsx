10f88f6acee80d744ab221be05a68ddfe873d877
Iulia <nedelcuniulia@gmail.com>
2024-06-20 12:15:30 +0100

    Format code

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,33 +1,29 @@
-import { Suspense } from 'react';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import styles from './Layout.module.css';
-import { Outlet } from 'react-router-dom';
-import Loader from '../Loader';
-import Navigation from '../Navigation/Navigation';
-import { navLinks } from '../../routes/routes';
-
+import { Suspense } from "react";
+import Header from "../Header/Header";
+import styles from "./Layout.module.css";
+import { Outlet } from "react-router-dom";
+import Loader from "../Loader";
+import Navigation from "../Navigation/Navigation";
+import { navLinks } from "../../routes/routes";
 
 const Layout = () => {
   return (
     <div className={styles.layout}>
+      <Header />
 
-    <Header />
-    
-    <div className={styles.menu__container}>
-      <Navigation pages={navLinks} />  
-    </div>
+      <div className={styles.menu__container}>
+        <Navigation pages={navLinks} />
+      </div>
 
-    <div className={styles.main__content}>
-
-      <Suspense fallback={<Loader />}>
+      <div className={styles.main__content}>
+        <Suspense fallback={<Loader />}>
           <Outlet />
-      </Suspense>
-    </div>
+        </Suspense>
+      </div>
 
-    {/* <Footer /> */}
+      {/* <Footer /> */}
     </div>
   );
-}
+};
 
 export default Layout;
